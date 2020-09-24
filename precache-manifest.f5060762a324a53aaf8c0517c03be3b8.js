d4e857f76313defefd0ce10f300f0ed3745adc7c
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-09-24 20:22:49 +0400

    Updates

---
+++ precache-manifest.f5060762a324a53aaf8c0517c03be3b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90a60bfdd3ceff3fb8d358a331cc456",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "2ba0d570d89d78ddc946",
+    "url": "/notes-app/static/css/main.9aa809eb.chunk.css"
+  },
+  {
+    "revision": "0191e6c0316ded35d023",
+    "url": "/notes-app/static/js/2.7e45b5dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notes-app/static/js/2.7e45b5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba0d570d89d78ddc946",
+    "url": "/notes-app/static/js/main.1e78c0b2.chunk.js"
+  },
+  {
+    "revision": "df6bc949055d9b1a1203",
+    "url": "/notes-app/static/js/runtime-main.f8fd9a4a.js"
+  },
+  {
+    "revision": "ccfa0cf608de229464273130e93d20eb",
+    "url": "/notes-app/static/media/clipboard.ccfa0cf6.svg"
+  }
+]);
